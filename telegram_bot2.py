e4afebc3043342261c55c6973c7c91513b1dd218
BorisTitarenko <specmohnat2309@gmail.com>
2017-08-22 18:39:47 +0300

    work

--- telegram_bot2.py
+++ telegram_bot2.py
@@ -8,12 +8,10 @@ bot = telebot.TeleBot(config.token)
 PORT = int(os.environ.get('PORT', '5000'))
 updater = Updater(config.token)
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
 
 def start(self, update):
 	sent = bot.send_message(message.chat.id, 'What do you want?')
 	
-updater.dispatcher.add_handler(CommandHandler('weather', start))
 		
 def weather(self, update):
 	bot.send_message(message.chat.id, "print a city")
@@ -23,7 +21,12 @@ def weather_1(self, update):
 	city = update.message
 	for a in  config.weather(city):
 		bot.send_message(message.chat.id, a)
+		
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('weather', start))
 
-updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-updater.bot.set_webhook("https://weatherbot.herokuapp.com/" + TOKEN)
+
+
+updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=config.token)
+updater.bot.set_webhook("https://weatherbot.herokuapp.com/" + config.token)
 updater.idle()
